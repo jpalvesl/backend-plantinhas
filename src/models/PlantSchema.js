2c2cac8bf2c819da4ae37fd0beaf024f1bd4c702
João Lima <jpalvesl101@gmail.com>
2020-02-22 15:43:45 +0000

    fazendo com que todas as informações fiquem num unico modelo schema

--- src/models/PlantSchema.js
+++ src/models/PlantSchema.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
-const PointSchema = require('./utils/PointSchema')
 
 const PlantSchema = new mongoose.Schema({
   name: String,
   diameter: Number,
   height: Number,
-  location: {
-    type: PointSchema,
-    index: '2dsphere'
-  }
+  latitude: Number,
+  longitude: Number,
+  date: Date
 })
 
 module.exports = mongoose.model('Plant', PlantSchema)
