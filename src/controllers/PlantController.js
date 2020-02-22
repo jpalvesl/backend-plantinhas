9f319c5174abf89e1baad9cff2c73bf2743ef466
João Lima <jpalvesl101@gmail.com>
2020-02-22 15:44:31 +0000

    adicionando os novos campos que o objeto planta recebe

--- src/controllers/PlantController.js
+++ src/controllers/PlantController.js
@@ -4,13 +4,19 @@ const Plant = require('../models/PlantSchema');
 // exporta modulos contendo os metodos
 module.exports = {
     async store(req, res) {
-        const { name, diameter, height } = req.body
-
+        const { name, diameter, height, latitude, longitude } = req.body
+        const date = new Date()
+        
+        
         const plant = await Plant.create({
             name,
             diameter,
-            height
+            height,
+            latitude,
+            longitude,
+            date
         })
+
         return res.json(plant)
     },
 
