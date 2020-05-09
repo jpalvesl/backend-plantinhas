89af12d33324d8dde971a2dd37b884f05d767f8e
João Lima <jpalvesl101@gmail.com>
2020-05-09 08:03:02 +0000

    preparando para deploy

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { errors } = require('celebrate');
 
 const routes = require('./routes');
 
 const app = express()
 
-mongoose.connect(`mongodb+srv://omnistack:omnistack@cluster0-6caix.mongodb.net/plantinhas?retryWrites=true&w=majority`, {
+mongoose.connect(process.env.MONGO_URL || `mongodb+srv://omnistack:omnistack@cluster0-6caix.mongodb.net/plantinhas?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
 
-
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(errors())
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
